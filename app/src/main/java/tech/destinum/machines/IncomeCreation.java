2824673116d000297a99699b0b7add0cc218f4c4
Diego Fernando Urrea <diegoug@gmail.com>
2017-01-26 11:59:44 -0500

    Working on Income inputs

--- app/src/main/java/tech/destinum/machines/IncomeCreation.java
+++ app/src/main/java/tech/destinum/machines/IncomeCreation.java
@@ -1,18 +1,24 @@
 package tech.destinum.machines;
 
+import android.app.DatePickerDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class IncomeCreation extends AppCompatActivity {
 
     private EditText mMoney, mNotes;
     private DatePicker mDate;
-    private FloatingActionButton mFAB;
+    private Calendar mCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,37 @@ public class IncomeCreation extends AppCompatActivity {
         mMoney = (EditText) findViewById(R.id.etMoney);
         mNotes = (EditText) findViewById(R.id.etNotes);
         mDate = (DatePicker) findViewById(R.id.datePicker);
-        mFAB = (FloatingActionButton) findViewById(R.id.fabAddIncome2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_confirmation, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.confirmation:
+                String notes = mNotes.getText().toString();
+                Double money = Double.valueOf(mMoney.getText().toString());
+                DatePickerDialog.OnDateSetListener datePickerDialog = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mCalendar = Calendar.getInstance();
+                        mCalendar.set(year, month, dayOfMonth);
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                        String formattedDate = sdf.format(mCalendar.getTime());
+                    }
+                };
+
+                if (money.equals("")|| money <= 0){
+
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/tech/destinum/machines/MachinesAdapter.java
+++ app/src/main/java/tech/destinum/machines/MachinesAdapter.java
@@ -1,18 +1,13 @@
 package tech.destinum.machines;
 
-import android.app.Fragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
