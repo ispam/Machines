3d0cd8c822ce94cf65569c9cfaea742d6805ea17
Diego <diegoug@gmail.com>
2017-09-06 18:42:59 -0500

    Improved whole proyect structure, going to start aesthetics

--- app/src/main/java/tech/destinum/machines/ACTIVITIES/MachineInfo.java
+++ app/src/main/java/tech/destinum/machines/ACTIVITIES/MachineInfo.java
@@ -1,22 +1,32 @@
 package tech.destinum.machines.ACTIVITIES;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 import java.text.DecimalFormat;
+import java.util.Calendar;
 
 import tech.destinum.machines.ADAPTERS.ListAdapter;
 import tech.destinum.machines.DB.DBHelpter;
 import tech.destinum.machines.R;
 
-public class MachineInfo extends AppCompatActivity {
+public class MachineInfo extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     private TextView mName, mMoney;
     private DBHelpter mDBHelpter;
@@ -24,6 +34,10 @@ public class MachineInfo extends AppCompatActivity {
     private FloatingActionButton mFAB;
     private ListAdapter mAdapter;
     private Context mContext;
+    private Calendar mCalendar;
+    private TextView info_date;
+    private int mDay, mMonth, mYear, mDayFinal, mMonthFinal, mYearFinal;
+    private static final String PREFS_NAME = "MachineInfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,11 +69,57 @@ public class MachineInfo extends AppCompatActivity {
 
         mFAB.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), IncomeCreation.class);
-                startActivity(i);
+            public void onClick(final View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                LayoutInflater inflater = (LayoutInflater) v.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.dialog_info, null, true);
+                EditText editText = (EditText) view.findViewById(R.id.dialog_info_et);
+                EditText editText2 = (EditText) view.findViewById(R.id.dialog_info_notes_et);
+                info_date = (TextView) view.findViewById(R.id.dialog_info_date_tv);
+                Button button = (Button) view.findViewById(R.id.dialog_info_date_btn);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCalendar = Calendar.getInstance();
+                        mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
+                        mMonth = mCalendar.get(Calendar.MONTH);
+                        mYear = mCalendar.get(Calendar.YEAR);
+
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(MachineInfo.this, MachineInfo.this, mYear, mMonth, mDay);
+                        datePickerDialog.show();
+                    }
+                });
+                dialog.setNegativeButton("Cancelar", null).setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+//                        mDBHelpter.insertNewIncome();
+
+//                        View view = v.getRootView();
+//                        if (view != null) {
+//                            InputMethodManager inputManager = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+//                            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+//                        }
+
+                    }
+                }).setView(view).show();
             }
         });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        mDayFinal = dayOfMonth;
+        mMonthFinal = month + 1;
+        mYearFinal = year;
+
+        SharedPreferences mSharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+        String date = mDayFinal+"/"+mMonthFinal+"/"+mYearFinal;
+        mEditor.putString("date", date);
+        mEditor.commit();
+
+        info_date.setText(date);
     }
 }
 
