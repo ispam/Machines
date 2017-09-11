b9d249e9301e5bdf18b2018bdcf6e67bf4d910e1
Diego <diegoug@gmail.com>
2017-09-11 17:23:00 -0500

    Using MPAndroidChartLibrary

---
+++ app/src/main/java/tech/destinum/machines/ACTIVITIES/Graph.java
@@ -0,0 +1,15 @@
+package tech.destinum.machines.ACTIVITIES;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.destinum.machines.R;
+
+public class Graph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
